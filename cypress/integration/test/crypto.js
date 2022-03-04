acef2b41ed7cf9345c78207161decce2022ff11b
Tommy <yourng2794@gmail.com>
2022-03-04 14:03:16 +0800

    cypress proj for tech test

---
+++ cypress/integration/test/crypto.js
@@ -0,0 +1,43 @@
+// <reference types="cypress" />
+describe("Web Portal Test", () => {
+    beforeEach(() => {
+      cy.visit("https://crypto.com/nft");
+    });
+
+    it.only("Test cypress", () => {
+      // Marketplace default load 6 cards
+      cy.get('a[data-test-id="nav-marketplace"').click();
+      cy.get('div[data-test-id="nftCard-image"]')
+      .should('have.length', 6);
+
+      //Marketplace mostview will sort by like desc
+      cy.get('button[data-test-id="marketplace-sort-button"]')
+      .click();
+      cy.get('button[data-test-id="marketplace-sort-likes"]')
+      .click();
+
+      var number_of_likes=[]
+      var i = 0
+      cy.get('div[data-test-id="nftCard-likes-count"]').each(($value) => {
+        number_of_likes.push($value.text());
+        if(number_of_likes.length>1){
+          cy.log(number_of_likes[i-1])
+          cy.log(number_of_likes[i])
+          expect(number_of_likes[i-1] >= number_of_likes[i]);
+        }
+        i= i+1
+      });
+
+      //Marketplace search one card and compare its' price
+      cy.get('input[data-test-id="nav-search-box-input"]').type('AlphaBot #9867{enter}');
+      cy.get('div[data-test-id="nftCard-image"]').click();
+      cy.get('div[data-test-id="listing-asset-name"]')
+      .should(($card_name) => {
+        expect($card_name).to.have.text('AlphaBot #9867');
+      });
+      cy.get('div[data-test-id="nftDetail-price"]')
+      .should(($price) => {
+        expect($price).to.have.text('999,999,999');
+      });
+    });
+  });

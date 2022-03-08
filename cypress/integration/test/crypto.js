b0256ca9bfcf7aab1bd1a2bef5c7f370121adf7f
Tommy <yourng2794@gmail.com>
2022-03-08 16:18:23 +0800

    enhance selectors to avoid use same selector in different lines

--- cypress/integration/test/crypto.js
+++ cypress/integration/test/crypto.js
@@ -1,24 +1,26 @@
 // <reference types="cypress" />
+import selectors from '../../support/selectors.js';
+
 describe("Web Portal Test", () => {
-    beforeEach(() => {
-      cy.visit("https://crypto.com/nft");
-      cy.get('a[data-test-id="nav-marketplace"').click()
+    before(() => {
+        cy.visit("https://crypto.com/nft")
+        cy.get(selectors.nav_marketplace).click()
     })
 
     it("Marketplace default load 6 cards", () =>{
-      cy.get('div[data-test-id="nftCard-image"]')
-      .should('have.length', 6)
+        cy.get(selectors.nftCard_image)
+        .should('have.length', 6)
     })
 
     it("Marketplace mostview will sort by like desc", () =>{
-        cy.get('button[data-test-id="marketplace-sort-button"]')
+        cy.get(selectors.marketplace_sort_button)
         .click()
-        cy.get('button[data-test-id="marketplace-sort-likes"]')
+        cy.get(selectors.marketplace_sort_likes)
         .click()
   
         var number_of_likes=[]
         var i = 0
-        cy.get('div[data-test-id="nftCard-likes-count"]').each(($value) => {
+        cy.get(selectors.nftCard_likes_count).each(($value) => {
           number_of_likes.push($value.text())
           if(number_of_likes.length>1){
             cy.log(number_of_likes[i-1])
@@ -30,16 +32,16 @@ describe("Web Portal Test", () => {
     })
 
 
-    it("Marketplace search one card and compare its' price", () => {
-      cy.get('input[data-test-id="nav-search-box-input"]').type('AlphaBot #9867{enter}')
-      cy.get('div[data-test-id="nftCard-image"]').click()
-      cy.get('div[data-test-id="listing-asset-name"]')
-      .should(($card_name) => {
+    it("Marketplace search one card and compare its' price", function() {
+        cy.get(selectors.nav_search_box_input).type('AlphaBot #9867{enter}')
+        cy.get(selectors.nftCard_image).click()
+        cy.get(selectors.listing_asset_name)
+        .should(($card_name) => {
         expect($card_name).to.have.text('AlphaBot #9867')
-      })
-      cy.get('div[data-test-id="nftDetail-price"]')
-      .should(($price) => {
+        })
+        cy.get(selectors.nftDetail_price)
+        .should(($price) => {
         expect($price).to.have.text('500')
-      })
+        })
     })
 })
---
+++ cypress/support/selectors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    nav_marketplace: 'a[data-test-id="nav-marketplace"]',
+    nftCard_image: 'div[data-test-id="nftCard-image"]',
+    marketplace_sort_button: 'button[data-test-id="marketplace-sort-button"]',
+    marketplace_sort_likes: 'button[data-test-id="marketplace-sort-likes"]',
+    nftCard_likes_count: 'div[data-test-id="nftCard-likes-count"]',
+    nav_search_box_input: 'input[data-test-id="nav-search-box-input"]',
+    listing_asset_name: 'div[data-test-id="listing-asset-name"]',
+    nftDetail_price: 'div[data-test-id="nftDetail-price"]'
+  }
